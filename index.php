2aa19fa10bcea5944e8f831926bf52de82513c49
NginxDragon <nginxdragon@gmail.com>
2020-06-17 17:25:50 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /* This will give an error. Note the output
  * above, which is before the header() call */
-header('Location: circleci.com/index.php');
+header('Location: support');
 exit;
 ?>
